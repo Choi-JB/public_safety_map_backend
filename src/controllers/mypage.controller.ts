bb5048cf406c9471bdd09f4eefb44b440987dbac
Choi-JB <jbong225@gmail.com>
2026-08-04 13:59:03 +0900

    mypage 조회시 jwt accesstoken 확인하는 로직 추가

--- src/controllers/mypage.controller.ts
+++ src/controllers/mypage.controller.ts
@@ -7,9 +7,11 @@ import prismaClient from "../config/prismaClient";
 /** 마이페이지 정보 조회 */
 export const getMypage = async (req: Request, res: Response): Promise<Response> => {
   try {
+    const userId = BigInt((req as any).user.id);
+
     //유효성 검사
     const user = await prismaClient.user.findUnique({
-      where: { id: BigInt(req.body.userId) },
+      where: { id: userId },
     });
     if (!user) {
       return res.status(404).json({ success: false, message: "User not found" });
@@ -41,10 +43,10 @@ export const getMyReportList = async (req: Request, res: Response): Promise<Resp
     //페이지, 한 페이지당 아이템 수, 필터, 날짜 범위(일수) (기본값: 30일)
     const { page = 1, limit = 10 } = req.query;
     const skip = (Number(page) - 1) * Number(limit);
-
+    const userId = BigInt((req as any).user.id);
     //내가 제보한 report 목록 조회
     const reports = await prismaClient.report.findMany({
-      where: { user_id: BigInt(req.body.userId) },
+      where: { user_id: userId },
       skip,
       take: Number(limit),
       orderBy: {
@@ -73,10 +75,10 @@ export const getMyFeedbackList = async (req: Request, res: Response): Promise<Re
     //페이지, 한 페이지당 아이템 수, 필터, 날짜 범위(일수) (기본값: 30일)
     const { page = 1, limit = 10 } = req.query;
     const skip = (Number(page) - 1) * Number(limit);
-
+    const userId = BigInt((req as any).user.id);
     //내가 제보한 feedback 목록 조회
     const feedbacks = await prismaClient.feedback.findMany({
-      where: { user_id: BigInt(req.body.userId) },
+      where: { user_id: userId },
       skip,
       take: Number(limit),
       orderBy: {
--- src/routes/mypage.routes.ts
+++ src/routes/mypage.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { getMypage, getMyReportList, getMyFeedbackList } from "../controllers/mypage.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 //GET /mypage — 마이페이지 정보 조회
-router.post("/", getMypage);
+router.get("/", authMiddleware, getMypage);
 //GET /mypage/report — 내가 제보한 report 목록 조회
-router.post("/report", getMyReportList);
+router.get("/report", authMiddleware, getMyReportList);
 //GET /mypage/feedback — 내가 제보한 feedback 목록 조회
-router.post("/feedback", getMyFeedbackList);
+router.get("/feedback", authMiddleware, getMyFeedbackList);
 
 export default router;
